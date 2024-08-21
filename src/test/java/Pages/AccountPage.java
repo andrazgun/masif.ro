f515ac0b1ad542c1cc0635125c5835d294cb44e1
Andrei Gunta <andrei.gunta@agilenetworks.tech>
2024-08-21 16:02:23 +0300

    Added AccountPage.java, AccountEditPage.java, AccountEditTest.java

--- src/test/java/Pages/AccountPage.java
+++ src/test/java/Pages/AccountPage.java
@@ -1,13 +1,29 @@
 package Pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 
-public class AccountPage extends HomePage {
+import static Utils.BrowserUtils.clickSubmitButtonByCss;
 
+public class AccountPage extends BasePage {
+    private final String pagePath = "/customer/account/";
     private String pageURL = "https://masif.ro/customer/account/";
-
-
+    private String editAccountPageSelector = "#maincontent > div.columns > div.column.main > div.block.block-dashboard-info > div.block-content > div.box.box-information > div.box-actions > a.action.edit";
     public AccountPage(WebDriver driver) {
         super(driver);
     }
+
+    public String setPagePath() {
+        return this.pagePath;
+    }
+    public void verifyPageURL() {
+        System.out.println("Page URL is: " + getPageURL());
+        Assert.assertEquals(getPageURL(), pageURL);
+    }
+    public void goToEditAccountPage() {
+        driver.findElement(By.cssSelector(editAccountPageSelector)).click();
+    }
+
 }
---
+++ src/test/java/Tests/AccountEditTest.java
@@ -0,0 +1,34 @@
+package Tests;
+
+import Pages.AccountEditPage;
+import Pages.AccountPage;
+import Pages.HomePage;
+import Pages.LoginPage;
+import Utils.GenericUtils;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AccountEditTest extends BaseTest {
+    @Test(
+            description = "Edit firstName and lastName",
+            groups = {"Regression"}
+    )
+    public void AccountEditTest01() {
+        driver.get(baseUrl);
+        HomePage homePage = new HomePage(driver);
+        homePage.verifyPageURL();
+        homePage.goToLoginPage();
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.verifyPageURL();
+        Assert.assertTrue(loginPage.getCheckboxNameSelector().isDisplayed());
+        loginPage.login("agtest1@yopmail.com", "Test1234");
+        AccountPage accountPage = new AccountPage(driver);
+        accountPage.verifyPageURL();
+        accountPage.goToEditAccountPage();
+        AccountEditPage accountEditPage = new AccountEditPage(driver);
+        accountEditPage.verifyPageURL();
+        accountEditPage.editAccount(GenericUtils.createRandomStringTwo(7),GenericUtils.createRandomStringTwo(7));
+        accountPage.verifyPageURL();
+    }
+
+}
